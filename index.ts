ff843054ffdf969cf36f4734b940584476eee423
alaa.ali <alaa.yahya.ali@gmial.com>
2026-06-08 01:58:51 +0200

    feat: add addTask function

--- index.ts
+++ index.ts
@@ -6,9 +6,20 @@ interface Task {
 }
 
 let tasks: Task[] = [];
+let id = 0;
 
 const TasksList: () => void = () => {
   console.log(tasks);
+};
+
+const addTask: (title: string) => void = (title) => {
+  const task: Task = {
+    id: id,
+    title: title,
+    isCompleted: false,
+  };
+  id += 1;
+  tasks.push(task);
 };
 
 TasksList();
