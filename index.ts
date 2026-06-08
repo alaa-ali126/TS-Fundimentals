83ca55b76561b6d83b4304c4b9fed6386986e048
alaa.ali <alaa.yahya.ali@gmial.com>
2026-06-08 02:03:39 +0200

    feat: add isCompleted function

--- index.ts
+++ index.ts
@@ -22,4 +22,19 @@ const addTask: (title: string) => void = (title) => {
   tasks.push(task);
 };
 
+const isCompleted: (id: number) => void = (id) => {
+  tasks = tasks.map((task) => {
+    return task.id === id ? { ...task, isCompleted: true } : { ...task };
+  });
+};
+
+TasksList();
+addTask("hello");
+addTask("hello");
+TasksList();
+addTask("hello");
+addTask("hello");
+addTask("hello");
+isCompleted(1)
+isCompleted(3)
 TasksList();
